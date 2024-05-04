19ca11a0a9ac2f4acd881d624c48c917ff436599
lizethmamani <lizvanesa09m@gmail.com>
2024-05-03 21:30:24 -0400

    Subiendo proyecto

---
+++ script.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    $("#menos").click(function(){
+        //obtiene el valor del contador y al mismo tiempo lo convierte en numero entero
+        var c = parseInt($("#contador").text());
+        //if(c>0){}
+        //incrementa el valor del contador y al mismo tiempo asigna el nuevo valor
+        $("#contador").text(c-1)
+    });
+    $("#mas").click(function(){
+        //obtiene el valor del contador y al mismo tiempo lo convierte en numero entero
+        var c = parseInt($("#contador").text());
+        //if(c<10){ }
+        //incrementa el valor del contador y al mismo tiempo asigna el nuevo valor
+        $("#contador").text(c+1)
+    });
+    $("#reset").click(function(){
+        //asigna el valor del contador en 0
+        $("#contador").text(0)
+    });
+});
